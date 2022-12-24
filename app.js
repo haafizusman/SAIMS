dc6d4b81992ba2806fe7638d2c904da476e90d10
haafizusman <121035086+haafizusman@users.noreply.github.com>
2022-12-24 17:55:22 +0500

    SAIMS
    SAIMS

---
+++ app.js
@@ -0,0 +1,84 @@
+// let arr1 = ["peter","musshy","jhon"];
+
+// let arr2 = ["jimmy","sam"];
+
+// console.log(...arr1,...arr2)
+
+
+// let obj = Object.assign([] ,{
+//     name: "peter"
+// })
+
+// console.log(obj["name"])
+
+// let arr = [1,2,3,4,5,6,7,8,9,10,["ali","mann","kazmi"]];
+
+// let arr1 = ["a","b","c","d"];
+
+// let arr2 = [...arr[10],...arr1];
+
+// console.log(arr2)
+
+
+
+// let a = (...a)=>{
+//     console.log(a)
+// }
+
+// a(1,2,3,4,5,6,7,8,9,10)
+
+// let a = [
+//     1,
+//     2,
+//     "a",
+//     "b",
+//     {
+//       id: 1,
+//       name: "ABC",
+//       details: [
+//         "hg1",
+//         "hg2",
+//         {
+//           id: 2,
+//           fullName: "hg ABC",
+//         },
+//       ],
+//     },
+//   ];
+
+
+//   let [,,,,a1] = a
+
+// //   console.log(details)
+
+// //   let{details} = a1
+
+//   console.log(a1)
+// //   console.log(details)
+
+
+
+
+// let [val1,val2] = a[4].details;
+// let {fullname} = a[4].details[2];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
